4f3b5e549d6dda95886715b33a04479c697f52f4
navneetbhaltilak <bhaltilaknavneet@gmail.com>
2026-09-02 13:06:07 +0530

    Add readline_file_io.py - reading lines from files

---
+++ readline_file_io.py
@@ -0,0 +1,17 @@
+f=open('myfile.txt','r')
+i=0
+while True:
+    i=i+1
+    line=f.readline()
+    if not line:
+        break
+    m1=int(line.split(",")[0])
+    m2=int(line.split(",")[1])
+    m3=int(line.split(",")[2])
+    
+    print(f"Marks of student {i} in Math is : {m1}")
+    print(f"Marks of student {i} in English is : {m2}")
+    print(f"Marks of student {i} in OOPs is : {m3}")
+    print(line)
+  
+    
