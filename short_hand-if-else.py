68fcc7a15aed114f642cfa6b2386c25b7ff26c3d
navneetbhaltilak <bhaltilaknavneet@gmail.com>
2026-09-02 13:06:08 +0530

    Add short_hand-if-else.py - shorthand if-else examples

---
+++ short_hand-if-else.py
@@ -0,0 +1,8 @@
+#defining a variabkle a,b
+a=45
+b=56
+#here integrating short-hand if-else statement which 
+#easy to use and increase code readiability
+print(a)if a>b else print("=")if a==b else print(b)
+c=9 if a>b else 0
+print(c)
