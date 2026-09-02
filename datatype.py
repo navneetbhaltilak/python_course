e624497152cf6fedf15a334ff3188584076caaec
navneetbhaltilak <bhaltilaknavneet@gmail.com>
2026-09-02 13:04:43 +0530

    Add datatype.py - experimenting with Python data types

---
+++ datatype.py
@@ -0,0 +1,24 @@
+a=123
+print(a)
+b=98967445768988.56
+print(b)
+c="harry"
+print(c)
+d=True
+print(d)
+e=None
+print(e)
+f=a+b
+print(f)
+print("the type of a is",type(a))
+print("the type of b is",type(b))
+print("the type of c is",type(c))
+print("the type of d is",type(d))
+print("the type of e is",type(e))
+print("the type of f is",type(f))
+list1=[8,2.4,-4,5,"apple","banana"]
+print(list1)
+tuple1=("apple","banana","parrot","tiger","lion")
+print(tuple1)
+dict1={"name":"navneet","height":6.2,"age":19,"gender":"male"}
+print(dict1)
